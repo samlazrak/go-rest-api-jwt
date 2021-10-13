4aadad316ebb4a9e82c187e24be47f03549bd2cf
Sam Lazrak <lazrak13@uab.edu>
2021-10-13 13:46:05 -0500

    logout

--- models/user.go
+++ models/user.go
@@ -3,9 +3,10 @@ package models
 // struct similar to classes in other languages
 
 type User struct {
-	Id uint
-	Name string
-	Email string `gorm:"unique"`
+	// the `json:` part allows to change how it will show to the client
+	Id uint `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email" gorm:"unique"`
 	// change from string to []byte in order to use properly
-	Password []byte
+	Password []byte `json:"-"`
 }
