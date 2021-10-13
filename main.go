1850c1c0e3a13d192935bf4b3902d1f2b1d7532c
Sam Lazrak <lazrak13@uab.edu>
2021-10-13 13:25:59 -0500

    jwt cookie cors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 // if error change to v1 ( remove /v2 )
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"jwt/database"
 	"jwt/routes"
 )
@@ -11,6 +12,12 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	// cors allows for different port to access requests : ie app runs on 9001 vs server on 9000
+	app.Use(cors.New(cors.Config{
+		// With this frontend can get cookie and send it back
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
